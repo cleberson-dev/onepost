f1f2f67ae36b8e54e7066200af4a6872e251e0c7
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2019-08-26 18:36:39 -0300

    Configurado e inicializado view da aplicação (React)

---
+++ src/views/src/App.styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  font-family: 'Livvic', sans-serif;
+  background-color: #0E103D;
+  color: #F2D7EE;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Header = styled.header``;
---
+++ src/views/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Container, Header } from './App.styles';
+
+const App: React.FC = () => {
+  return (
+    <Container>
+      <Header>
+        <h1>Olá, mundo!</h1>
+        <h2>Meu nome é Cleberson.</h2>
+      </Header>
+    </Container>
+  );
+}
+
+export default App;
