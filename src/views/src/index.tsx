7e161e48120c1399a21f5399594b388d4a842edc
Cleberson F. R. Junior <cleberson.dev@gmail.com>
2019-12-31 08:17:41 -0300

    Adjusted the root 'index.tsx' file

--- src/views/src/index.tsx
+++ src/views/src/index.tsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { ThemeProvider } from 'styled-components';
 import ReactDOM from 'react-dom';
 import App from './App';
+import theme from './theme';
+import store from './store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </Provider>, 
+  document.getElementById('root')
+);
